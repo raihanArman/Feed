45d45e882b47331e0f09a8f5480c5eaa41074ec4
raihanArman <raihanarman8@gmail.com>
2023-11-18 19:38:21 +0700

    create cryptofeed data class

---
+++ app/src/main/java/com/raihanarman/feed/CryptoFeed.kt
@@ -0,0 +1,25 @@
+package com.raihanarman.feed
+
+/**
+ * @author Raihan Arman
+ * @date 18/11/23
+ */
+data class CryptoFeed(
+    val coinInfo: CoinInfo,
+    val raw: Raw
+)
+
+data class Raw(
+    val usd: Usd
+)
+
+data class Usd(
+    val price: Double,
+    val changePctDay: Long
+)
+
+data class CoinInfo(
+    val id: String,
+    val name: String,
+    val fullName: String
+)

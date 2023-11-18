1ca043ea51fa3b427dd33f19f8b7094f2d6203fe
raihanArman <raihanarman8@gmail.com>
2023-11-18 20:20:12 +0700

    Create `LoadCryptoFeedRemoteUseCaseTest`

---
+++ app/src/test/java/com/raihanarman/feed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -0,0 +1,16 @@
+package com.raihanarman.feed
+
+import org.junit.Test
+
+/**
+ * @author Raihan Arman
+ * @date 18/11/23
+ */
+class LoadCryptoFeedRemoteUseCaseTest {
+
+    @Test
+    fun testInit() {
+
+    }
+
+}

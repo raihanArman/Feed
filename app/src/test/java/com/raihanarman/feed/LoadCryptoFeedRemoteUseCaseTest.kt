d0d9c44bc538c258d114b472cdabd05a23e241e8
raihanArman <raihanarman8@gmail.com>
2023-11-18 21:12:04 +0700

    `LoadCryptoFeedRemoteUseCase` doesn't request data upon creation

--- app/src/test/java/com/raihanarman/feed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/raihanarman/feed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,16 +1,29 @@
 package com.raihanarman.feed
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
  * @author Raihan Arman
  * @date 18/11/23
  */
+
+class LoadCryptoFeedRemoteUseCase {
+
+}
+
+class HttpClient {
+    var getCount = 0
+}
+
 class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
-    fun testInit() {
+    fun testInitDoesNotLoad() {
+        val client = HttpClient()
+        val sut = LoadCryptoFeedRemoteUseCase()
 
+        assertTrue(client.getCount == 0)
     }
 
 }

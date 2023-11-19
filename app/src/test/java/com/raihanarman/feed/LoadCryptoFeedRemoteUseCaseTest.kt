542848187ab2dfb6877380b2ed305934a4d8cd0e
raihanArman <raihanarman8@gmail.com>
2023-11-19 12:56:26 +0700

    Request data once load

--- app/src/test/java/com/raihanarman/feed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/raihanarman/feed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.raihanarman.feed
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -9,10 +10,15 @@ import org.junit.Test
  */
 
 class LoadCryptoFeedRemoteUseCase {
-
+    fun load() {
+        HttpClient.instance.getCount = 1
+    }
 }
 
-class HttpClient {
+class HttpClient private constructor(){
+    companion object {
+        val instance = HttpClient()
+    }
     var getCount = 0
 }
 
@@ -20,10 +26,23 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient()
+        val client = HttpClient.instance
         val sut = LoadCryptoFeedRemoteUseCase()
 
         assertTrue(client.getCount == 0)
+    }
+
+    @Test
+    fun testLoadRequestData() {
+        // Given
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        // When
+        sut.load()
+
+        // Then
+        assertEquals(1, client.getCount)
     }
 
 }
